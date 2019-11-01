df83202dfd32e2fa62bc4c124dd4e88329226db6
Kaio Henrique <kaioh95@gmail.com>
2019-11-01 13:12:55 -0300

    Just an idea for the interface

--- src/br/imd/View/Main.java
+++ src/br/imd/View/Main.java
@@ -12,75 +12,6 @@ public class Main {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    /*
-
-    public static void k_nn(List<String[]> imgs, List<Float> imgx, int k){
-        int x = 1;
-        HeapTree rankedImgs = new HeapTree();
-
-        for(String[] img: imgs){
-            rankedImgs.addNode(distance(img, imgx), img);
-            System.out.println(x + " distance: " + distance(img, imgx) + " " + img[img.length-1]);
-            x += 1;
-        }
-
-        rankedImgs.heapSort();
-        System.out.println(rankedImgs.toString());
-        Node[] rank = new Node[3];
-        rank = rankedImgs.peek(k);
-        rankedImgs.fillNa();
-        for(Node node: rank){
-            System.out.println(node.toString());
-            node = null;
-        }
-    }
-
-    */
-
-    /*
-
-    public static double euclideanDistance(String[] img, List<Float> x){
-        double distance = 0;
-        for(int i=0; i < x.size(); i++){
-            double xi = x.get(i).doubleValue();
-            double yi = Double.parseDouble(img[i]);
-            distance += Math.pow(xi - yi, 2);
-        }
-        return Math.sqrt(distance);
-    }
-
-    */
-
-    /*
-
-
-    public static double manhattanDistance(String[] img, List<Float> x) {
-        double distance = 0;
-        for (int i = 0; i < x.size(); i++) {
-            double xi = x.get(i).doubleValue();
-            double yi = Double.parseDouble(img[i]);
-            distance += Math.abs(xi - yi);
-        }
-        return distance;
-    }
-
-    */
-
-    /*
-
-    public static double chebychevDistance(String[] img, List<Float> x) {
-        double distance = 0;
-        for (int i = 0; i < x.size(); i++) {
-            double xi = x.get(i).doubleValue();
-            double yi = Double.parseDouble(img[i]);
-            if( (xi - yi) > distance)
-                distance = xi - yi;
-        }
-        return distance;
-    }
-
-    */
-
     public static void main(String[] args) {
         CSVDataReader data = new CSVDataReader();
         data.readAllData(data.getCsv_path());
@@ -90,19 +21,6 @@ public class Main {
 
         KNNAlgorithm k_nn = new KNNAlgorithm();
         k_nn.k_nn(data.getDataset(), imgFeatures, 3, 1);
-
-        /*
-
-        HOGDescriptor hog = new HOGDescriptor();
-        Mat img = new Mat();
-        MatOfFloat features = new MatOfFloat();
-        img = Imgcodecs.imread("data/img1.png", Imgcodecs.IMREAD_GRAYSCALE);
-        Imgproc.resize(img, img, new Size(64, 128), 0.5, 0.5, Imgproc.INTER_LINEAR);
-        hog.compute(img, features);
-        List<Float> arrayOfFeatures = features.toList();
-        System.out.println(arrayOfFeatures.toString());
-
-        */
     }
 
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Label actionLabel;
+
+    @FXML
+    private TextField filePath;
+
+    @FXML
+    private Button filePathB;
+
+    @FXML
+    private ComboBox<?> distanceChooser;
+
+    @FXML
+    private Button runB;
+
+    //The methods of each attribute needed to be implemented.
 }
